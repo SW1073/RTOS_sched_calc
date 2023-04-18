37b5f80a9338c14ab20d219d54b5cace980f6963
SW1073 <eloiimerino@gmail.com>
2023-04-19 00:43:48 +0200

    Barebones de script1

--- script1/src/main.rs
+++ script1/src/main.rs
@@ -1,3 +1,48 @@
+struct Task {
+    computing_time: f64,
+    deadline: usize,
+    period: usize,
+}
+
+impl Task {
+
+    /**
+     * Constructora default
+     */
+    fn new(computing_time: f64, deadline: usize, period: usize) -> Self {
+        Task {
+            computing_time,
+            deadline,
+            period,
+        }
+    }
+}
+
+struct CyclicScheduler {
+    tasks: Vec<Task>,
+}
+
+impl CyclicScheduler {
+
+    /**
+     * Constructora default
+     */
+    fn new() -> Self {
+        CyclicScheduler {
+            tasks: vec![],
+        }
+    }
+
+    /**
+     * Afegeix una nova tasca al planificador
+     */
+    fn add_task(&mut self, computing_time: f64, deadline: usize, period: usize) -> Result<(), String> {
+        self.tasks.push(Task::new(computing_time, deadline, period));
+        Ok(())   
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut cs = CyclicScheduler::new();
+    cs.add_task(1.0, 10, 10).unwrap();
 }
