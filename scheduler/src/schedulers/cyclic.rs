81bed84476181f50ad6742e5e52bde304054e38c
SW1073 <eloiimerino@gmail.com>
2023-04-29 23:53:58 +0200

    Reformas. MEGA reformas

--- scheduler/src/schedulers/cyclic.rs
+++ scheduler/src/schedulers/cyclic.rs
@@ -1,7 +1,9 @@
-use scheduler::task::Task;
+use crate::task::Task;
+use super::CheckSchedulable;
+use super::lcm;
 
 #[derive(Debug)]
-struct CyclicScheduler {
+pub struct CyclicScheduler {
     tasks: Vec<Task>,
 }
 
@@ -10,7 +12,7 @@ impl CyclicScheduler {
     /**
      * Constructora default
      */
-    fn new() -> Self {
+    pub fn new() -> Self {
         CyclicScheduler {
             tasks: vec![],
         }
@@ -19,70 +21,9 @@ impl CyclicScheduler {
     /**
      * Afegeix una nova tasca al planificador
      */
-    fn add_task(&mut self, computing_time: f64, deadline: usize, period: usize) -> Result<(), String> {
+    pub fn add_task(&mut self, computing_time: f64, deadline: usize, period: usize) -> Result<(), String> {
         self.tasks.push(Task::new(computing_time, deadline, period));
         Ok(())   
-    }
-
-    /**
-     * Retorna si la tasca es planificable o no
-     */
-    fn is_schedulable(&self) -> bool {
-        // Check utilization factor
-        println!("# Busquem el factor d'utilitzacio");
-        let u = self.get_utilization();
-        println!("El factor d'utilitzacio U = {u}");
-        if u > 1.0 {
-            println!("! Com que U > 1.0, el sistema no es planificable.");
-            return false
-        };
-        println!("Com que U <= 1.0, continuem mirant condicions.");
-
-        // Find the hyperperiod
-        let hyper_period = self.get_hyperperiod();
-        println!("# L'hiperperiode H = {hyper_period}");
-
-        // Secondary period
-        println!("# A continuació, trobem el periode secondari:");
-        // Find max computing time
-        let max_c = self.get_max_computing_time();
-        println!("El temps de comput màxim és: {max_c}");
-        // Find min deadline time
-        let min_d = self.get_min_deadline();
-        println!("El deadline mínim és: {min_d}");
-        if max_c >= (min_d as f64) {
-            println!("! Com que el temps de comput màxim és major o igual que el mínim deadline, no es possible planificar aquest sistema actualment.");
-            return false;
-        }
-        println!("Com que el temps de comput màxim és menor que el mínim deadline, és possible trobar frames secuandaris en el rang.");
-
-        println!("De fet, gracies a la equivalencia H = k*Ts, sabem que:");
-        let kd = hyper_period/min_d;
-        let kc = (hyper_period as f64)/max_c;
-        println!("H = k*Ts = {kd} * {min_d} = {hyper_period}");
-        println!("H = k*Ts = {kc} * {max_c} = {hyper_period}");
-
-        let mult: usize = CyclicScheduler::get_multiplier(max_c);
-
-        // ⩝𝑖: 2𝑇𝑠 − gcd(𝑇𝑠, 𝑇𝑖) ≤ 𝐷𝑖
-        // With both values of 𝑇𝑠
-        println!("# Trobem el frame secundari:");
-        let tsd = min_d;
-        let tsc = (max_c * mult as f64) as usize;
-        let mut i = 1;
-        for t in self.tasks.iter() {
-            println!("Tasca numero {i}. ");
-            if 2*tsd - gcd(tsd, t.get_period()) > t.get_deadline() {
-                return false; 
-            }
-            if 2*tsc - gcd(tsc, t.get_period()*mult) > t.get_deadline()*mult {
-                return false; 
-            }
-            i += 1;
-        }
-
-        // return true if every check before was ok
-        true
     }
 
     /**
@@ -116,5 +57,52 @@ impl CyclicScheduler {
     fn get_min_deadline(&self) -> usize {
         return self.tasks.iter().map(|t| t.get_deadline()).min_by(|a, b| a.cmp(&b)).unwrap_or(0);
     }
+}
+
+/**
+ * Implementació del common trait IsSchedulable, que conté la funció is_schedulable()
+ */
+impl CheckSchedulable for CyclicScheduler {
+    fn is_schedulable(&mut self) -> bool {
+        // Check utilization factor
+        println!("# Busquem el factor d'utilitzacio");
+        let u = self.get_utilization();
+        println!("El factor d'utilitzacio U = {u}");
+        if u > 1.0 {
+            println!("! Com que U > 1.0, el sistema no es planificable.");
+            return false
+        };
+        println!("Com que U <= 1.0, continuem mirant condicions.");
+
+        // Find the hyperperiod
+        let hyper_period = self.get_hyperperiod();
+        println!("# L'hiperperiode H = {hyper_period}");
+
+        // Secondary period
+        println!("# A continuació, trobem el periode secundari:");
+        // Find max computing time
+        let max_c = self.get_max_computing_time();
+        println!("El temps de comput màxim és: {max_c}");
+        // Find min deadline time
+        let min_d = self.get_min_deadline();
+        println!("El deadline mínim és: {min_d}");
+        if max_c >= (min_d as f64) {
+            println!("! Com que el temps de comput màxim és major o igual que el mínim deadline, no es possible planificar aquest sistema actualment.");
+            return false;
+        }
+        println!("Com que el temps de comput màxim és menor que el mínim deadline, és possible trobar frames secuandaris en el rang.");
+
+        println!("De fet, gracies a la equivalencia H = k*Ts, sabem que:");
+        let kd = hyper_period/min_d;
+        let kc = (hyper_period as f64)/max_c;
+        println!("H = k*Ts = {kd} * {min_d} = {hyper_period}");
+        println!("H = k*Ts = {kc} * {max_c} = {hyper_period}");
+
+        // TODO: Cal trobar un layout de les tasques sobre el hyper_period i els secondary period.
+
+        // return true if every check before was ok
+        true
+    }
+    
 }
 
--- scheduler/src/schedulers/mod.rs
+++ scheduler/src/schedulers/mod.rs
@@ -1,2 +1,15 @@
+// Exposem els diferents schedulers per a que puguin ser utilitzats fora del crate.
 pub mod cyclic;
 pub mod monotonic;
+
+// Import global per a que tots els membres d'aquest modul puguin usar lcm i gcd.
+pub use num::integer::{lcm, gcd};
+
+
+/**
+ * Trait queassgura que qui l'implementi, te un mètode per a poder
+ * comprovar la seva planificabilitat.
+ */
+pub trait CheckSchedulable {
+    fn is_schedulable(&mut self) -> bool;
+}
