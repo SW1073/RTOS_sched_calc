2a4f4de63500de846aa605f5dff04cfa13d9496e
SW1073 <eloiimerino@gmail.com>
2023-05-08 17:15:09 +0200

    Multiplier eliminat. No era necesari

--- scheduler/src/task.rs
+++ scheduler/src/task.rs
@@ -14,14 +14,7 @@ pub struct Task {
     computing_time: f64,
     deadline: usize,
     period: usize,
-    multiplier: f64,
 }
-
-/**
- * Constant que defineix la maxima precisio que retenim quan cambiem la base del temps.
- * Preferiblement, ha de ser una potència de 10.
- */
-const MAX_MULT: f64 = 10000_f64;
 
 /**
  * Funcions de tasca
@@ -36,20 +29,7 @@ impl Task {
             computing_time,
             deadline,
             period,
-            multiplier: utils::find_mult(computing_time),
         }
-    }
-
-    // ========== INTERACCIO AMB EL MULTIPLIER ==========
-    /**
-     * Retorna el factor multiplicador de la tasca
-     */
-    pub fn get_multiplier(&self) -> usize {
-        self.multiplier as usize
-    }
-
-    pub fn set_multiplier(&mut self, mult: usize) {
-        self.multiplier = mult as f64;
     }
 
     // ========== GETTERS ==========
@@ -81,54 +61,11 @@ impl Task {
         self.period
     }
 
-    // ========== GETTERS AMB MULTIPLIER ==========
-    /**
-     * Retorna el temps de comput de la task * multiplier
-     */
-    pub fn get_computing_time_mult(&self) -> usize {
-        (self.computing_time * self.multiplier) as usize
-    }
-
-    /**
-     * Retorna el deadline de la task * multiplier
-     */
-    pub fn get_deadline_mult(&self) -> usize {
-        self.deadline * self.multiplier as usize
-    }
-
-    /**
-     * Retorna el periode de la task * multiplier
-     */
-    pub fn get_period_mult(&self) -> usize {
-        self.period * self.multiplier as usize
-    }
-
     /**
      * Divideix la tasca en 2. Es modifica a si mateixa i en retorna la nova tasca, amb computing_time/2
      */
     pub fn divide_task(&mut self) -> Task {
         self.computing_time /= 2.0;
         self.clone()
-    }
-}
-
-/**
- * Módul de funcions utilitaries
- */
-mod utils {
-    use super::MAX_MULT;
-    // ========== FUNCIONS PRIVADES ==========
-    /**
-     * Troba el mínim multiplier de base 10 pel que f deixa de tenir
-     * decimals. El multiplier tindrà valor 1.0 <= m <= MAX_MULT.
-     */
-    pub fn find_mult(f: f64) -> f64 {
-        let mut mult = 1.0;
-
-        while ((f * mult).ceil() != (f * mult)) && (mult < MAX_MULT) {
-            mult *= 10.0;
-        }
-
-        mult
     }
 }
